b0270d29acd2130b7d37751ab4b3e12fef69ee0b
draxxdk <draxxdk@gmail.com>
2017-08-21 22:18:47 +0200

    google maps fix

--- js/app/app.js
+++ js/app/app.js
@@ -76,17 +76,16 @@
 
         $body.css({ 'margin-right': 0, 'margin-bottom': 0 });
     }
-
-    function initMap() {
-        var uluru = { lat: -25.363, lng: 131.044 };
-        var map = new google.maps.Map(document.getElementById('map'), {
-            zoom: 4,
-            center: uluru
-        });
-        var marker = new google.maps.Marker({
-            position: uluru,
-            map: map
-        });
-    }
-    initMap();
 });
+
+function initMap() {
+    var uluru = { lat: -25.363, lng: 131.044 };
+    var map = new google.maps.Map(document.getElementById('map'), {
+        zoom: 4,
+        center: uluru
+    });
+    var marker = new google.maps.Marker({
+        position: uluru,
+        map: map
+    });
+}
--- js/app/form.js
+++ js/app/form.js
@@ -4,7 +4,7 @@
     }
 });
 
-$(document).ready(function () {
+$(function () {
     $("#signupForm").validate({
         rules: {
             firstname: "required",
