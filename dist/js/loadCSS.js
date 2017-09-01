de40b79628554784a2f4e1dad9edd7c2e8bb106a
drexxdk <draxxdk@gmail.com>
2017-09-01 03:55:28 +0200

    got source map for scss to work on localhost

--- dist/js/loadCSS.js
+++ dist/js/loadCSS.js
@@ -78,4 +78,9 @@
 	}
 }( typeof global !== "undefined" ? global : this ));
 
-loadCSS("dist/css/app.min.css");
+// source map is not generated for minified css
+var suffix = '.css';
+if (location.hostname !== "localhost" && location.hostname !== "127.0.0.1") {
+    suffix = ".min" + suffix;
+}
+loadCSS("dist/css/app" + suffix);
--- js/vendors/loadCSS/script.js
+++ js/vendors/loadCSS/script.js
@@ -1,1 +1,6 @@
-﻿loadCSS("dist/css/app.min.css");
+﻿// source map is not generated for minified css
+var suffix = '.css';
+if (location.hostname !== "localhost" && location.hostname !== "127.0.0.1") {
+    suffix = ".min" + suffix;
+}
+loadCSS("dist/css/app" + suffix);
