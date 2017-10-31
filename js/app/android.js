41acedc7091afec4f37608e417295bc6617520c7
Frederik NIelsen <draxxdk@gmail.com>
2017-10-31 21:49:53 +0100

    android fixes

--- js/app/android.js
+++ js/app/android.js
@@ -2,6 +2,23 @@
 
 $(function () {
     if (app.html.hasClass('android')) {
+
+        var setTouchAndSwipe = function (element, scrollElement = element, scrollTopElement = element) {
+            element.css("touch-action", "pan-down");
+            scrollElement.scroll(function () {
+                // this disables page refresh when swiping down
+                if (scrollTopElement.scrollTop() > 0) {
+                    element.css("touch-action", "auto");
+                } else {
+                    element.css("touch-action", "pan-down");
+                }
+            });
+            element.on('touchstart touchend click', '.hack', function (e) {
+                // this disables click after swiping
+                e.preventDefault();
+            });
+        }
+
         // android doesn't handle vh correctly, so it gets converted to px
         // might be a problem for ios also, but haven't tested it there yet
         app.fullscreen.img.css('max-height', window.innerHeight);
@@ -10,24 +27,9 @@ $(function () {
                 app.fullscreen.img.css('max-height', window.innerHeight);
             }
         });
-        
-        app.body.css("touch-action", "pan-down");
-        $(window).scroll(function () {
-            if ($(document).scrollTop() > 0) {
-                app.body.css("touch-action", "auto");
-            } else {
-                app.body.css("touch-action", "pan-down");
-            }
-        });
 
-        var aside = $(app.left, app.right).children('.content');
-        aside.css("touch-action", "pan-down");
-        aside.scroll(function () {
-            if (aside.scrollTop() > 0) {
-                aside.css("touch-action", "auto");
-            } else {
-                aside.css("touch-action", "pan-down");
-            }
-        });
+        setTouchAndSwipe(app.body, $(window), $(document));
+        setTouchAndSwipe(app.left.children('.content'));
+        setTouchAndSwipe(app.right.children('.content'));
     }
 });
