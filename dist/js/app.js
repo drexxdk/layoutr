1df5593593216c7bb4ac10c3535c31b683bc8722
Frederik NIelsen <draxxdk@gmail.com>
2017-11-06 22:24:26 +0100

    .

--- dist/js/app.js
+++ dist/js/app.js
@@ -1313,19 +1313,23 @@ $(function () {
         $.getScript("dist/js/touchDevice.js", function (data, textStatus, jqxhr) {
             app.body
                 .on('swipeleft', function (e) {
-                    var currentAside = app.html.attr('data-aside');
-                    if (currentAside === 'left' && currentAside !== 'right') {
-                        app.toggleAside();
-                    } else if (currentAside !== 'right') {
-                        app.toggleAside('right');
+                    if (!html.hasClass('scrollDisabled')) {
+                        var currentAside = app.html.attr('data-aside');
+                        if (currentAside === 'left' && currentAside !== 'right') {
+                            app.toggleAside();
+                        } else if (currentAside !== 'right') {
+                            app.toggleAside('right');
+                        }
                     }
                 })
                 .on('swiperight', function (e) {
-                    var currentAside = app.html.attr('data-aside');
-                    if (currentAside === 'right' && currentAside !== 'left') {
-                        app.toggleAside();
-                    } else if (currentAside !== 'left') {
-                        app.toggleAside('left');
+                    if (!html.hasClass('scrollDisabled')) {
+                        var currentAside = app.html.attr('data-aside');
+                        if (currentAside === 'right' && currentAside !== 'left') {
+                            app.toggleAside();
+                        } else if (currentAside !== 'left') {
+                            app.toggleAside('left');
+                        }
                     }
                 });
         });
