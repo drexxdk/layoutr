864492a928251da083a9ca1bd46d3a7bded8e69b
Frederik NIelsen <draxxdk@gmail.com>
2017-11-18 16:38:22 +0100

    moved edge/ie js to browser.js

--- js/app/_app.js
+++ js/app/_app.js
@@ -51,37 +51,6 @@ $(function () {
         app.html.addClass('desktop'); // enables hover effects
     }
 
-    if (app.html.hasClass('msie') || app.html.hasClass('msedge')) {
-        // disable smooth scrolling, since it causes element jumping/lagging on scroll
-        // https://stackoverflow.com/questions/29416448/how-to-disable-smooth-scrolling-in-ie11
-        app.body.on("mousewheel", function (e) {
-            var target = $(e.target);
-            if (!app.html.hasClass('modal') && event.ctrlKey !== true) {
-                var aside = target.closest('aside > .content') || target.parents('aside .content');
-                e.preventDefault();
-                var wheelDelta = e.originalEvent.wheelDelta;
-                if (aside.length) {
-                    var currentScrollPosition = aside.scrollTop();
-                    aside.scrollTop(currentScrollPosition - wheelDelta);
-                } else {
-                    var currentScrollPosition = window.pageYOffset;
-                    window.scrollTo(0, currentScrollPosition - wheelDelta);
-                }
-            }
-        });
-
-        // at some zoom levels edge/ie makes $(window) heigher than $(document)
-        // it causes a gap between footer and the bottom of $(window).
-        $(window).scroll(function () {
-            var scrollTop = self.pageYOffset || document.documentElement.scrollTop || document.body.scrollTop;
-            if (scrollTop + $(window).height() >= $(document).height()) {
-                app.html.addClass('subpixel');
-            } else {
-                app.html.removeClass('subpixel');
-            }
-        });
-    }
-
     if (bowser.android) {
         app.html.addClass('android'); // used by fullscreen
     } else if (bowser.ios) {
