520e72567bd16ad52beb2a1720e500b2c478226c
Frederik NIelsen <draxxdk@gmail.com>
2017-11-25 22:41:04 +0100

    added dropdown math example

--- js/app/dropdown.js
+++ js/app/dropdown.js
@@ -30,7 +30,12 @@ app.dropdown = function (dropdowns) {
         html.push('<ul class="' + theme + '">');
         $this.children(':not([value=""])').each(function (index) {
             var $that = $(this);
-            html.push('<li data-id="' + $that.val() + '"' + ($that.is(':selected') ? ' class="selected"' : '') + '><div tabindex="0" class="theme-light"><label>' + $that.text() + '</label><svg focusable="false"><use xlink:href="#svg-checkmark"></use></svg></div></li>');
+            var text = $that.text();
+            if (text.indexOf('$$') == 0) {
+                $that.attr('data-math', text);
+            }
+
+            html.push('<li data-id="' + $that.val() + '"' + ($that.is(':selected') ? ' class="selected"' : '') + '><div tabindex="0" class="theme-light"><label>' + text + '</label><svg focusable="false"><use xlink:href="#svg-checkmark"></use></svg></div></li>');
         });
         html.push('</ul>');
         html.push('</div>');
@@ -46,9 +51,19 @@ app.dropdown = function (dropdowns) {
             if (!$that.hasClass('selected')) {
                 $that.siblings('.selected').removeClass('selected');
                 $that.addClass('selected');
-                dropdown.children('div').children('label').text($that.text());
+                var option = $this.children('[value="' + $that.attr('data-id') + '"]');
+                var text = $that.text();
+                var math = option.attr('data-math');
+                if (math !== undefined) {
+                    text = math;
+                }
+                var label = dropdown.children('div').children('label');
+                label.text(text);
+                if (math !== undefined) {
+                    renderMathInElement(label[0]);
+                }
                 $this.children(':selected').removeAttr('selected');
-                $this.children('[value="' + $that.attr('data-id') + '"]').attr('selected', 'selected');
+                option.attr('selected', 'selected');
                 $this.change();
             }
             dropdown.removeClass('open');
