ab8fe6afb6492e045a837a06b814b74f94b5b77e
drexxdk <draxxdk@gmail.com>
2017-12-01 23:40:26 +0100

    removed ios slider-handle fix, since it didn't work. need ghostlab to fix it

--- dist/js/app.js
+++ dist/js/app.js
@@ -2603,8 +2603,6 @@ $(window).click(function (e) {
         // this forces :focus to be applied correctly.
         if (target.parents('button').length) {
             target.parents('button').focus();
-        } else if (target.parents('.slider').length) {
-            target.parents('.slider').focus();
         } else if (target.closest('button').length) {
             target.focus();
         }
