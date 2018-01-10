e69f06090c854f295008d1dc94d5d907620f1669
Frederik NIelsen <draxxdk@gmail.com>
2018-01-10 21:32:54 +0100

    refactoring

--- js/app/components/katex.js
+++ js/app/components/katex.js
@@ -2,12 +2,15 @@
 
 app.katex = function (katex) {
     if (katex.length) {
-        $.getScript('dist/js/katex.min.js', function () {
-            katex.each(function () {
-                var $this = $(this);
-                $.when(renderMathInElement($this[0])).done(function () {
-                    $this.removeClass('katex');
-                });
+        if (!app.html.hasClass('katex-loaded')) {
+            app.head.append($('<link rel="stylesheet"href="dist/css/katex.min.css">'));
+            app.body.append($('<script type="text/javascript" src="dist/js/katex.min.js">'));
+            app.html.addClass('katex-loaded');
+        }
+        katex.each(function () {
+            var $this = $(this);
+            $.when(renderMathInElement($this[0])).done(function () {
+                $this.removeClass('katex');
             });
         });
     }
