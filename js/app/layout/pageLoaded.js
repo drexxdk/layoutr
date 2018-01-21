0f7eb9c500f181960e4f8f9553b3761db16349f9
drexxdk <draxxdk@gmail.com>
2018-01-21 11:51:57 +0100

    android test

--- js/app/layout/pageLoaded.js
+++ js/app/layout/pageLoaded.js
@@ -4,7 +4,9 @@ app.pageLoaded = function (initial) {
     app.main.css('overflow', 'auto');
     app.main.scrollTop(0);
     app.main.css('overflow', '');
-    app.html.animate({ scrollTop: 0 }, 0);
+
+    // need to be fixed with ghostlab
+    //app.html.animate({ scrollTop: 0 }, 0);
     //if ($(document).scrollTop() > 0) {
     //    // android navigation bar offset fix
     //    app.html.animate({ scrollTop: -offsetTop }, 0);
