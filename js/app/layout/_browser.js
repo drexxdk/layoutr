12a894e0cfeb6f74d0d2d5367081ea6c2a8e2a24
Frederik Nielsen <draxxdk@gmail.com>
2019-01-13 13:34:39 +0100

    internet explorer page improved

--- js/app/layout/_browser.js
+++ js/app/layout/_browser.js
@@ -1,5 +1,15 @@
 ﻿if (window.navigator.userAgent.indexOf("MSIE") !== -1 || navigator.userAgent.match(/Trident\/7\./)) {
     document.documentElement.classList.add('browser-not-supported');
-    document.getElementsByTagName("body")[0].innerHTML = '<main><h1>Internet Explorer is not supported</h1><ul><li>Open Microsoft Edge</li><li><a href="https://www.google.com/chrome/">Download Google Chrome</a></li><li><a href="https://www.mozilla.org/firefox">Download Mozilla Firefox</a></li></ul></main>';
+    document.getElementsByTagName("body")[0].innerHTML =
+`<main>
+<h1>Internet Explorer is not supported</h1>
+<p>You're using a web browser we don't support.<br />
+Try one of these options to have a better experience on layoutr.</p>
+<ul>
+<li>Open Microsoft Edge</li>
+<li><a href="https://www.google.com/chrome/">Download Google Chrome</a></li>
+<li><a href="https://www.mozilla.org/firefox">Download Mozilla Firefox</a></li>
+</ul>
+</main>`;
     throw 'browser-not-supported';
 }
