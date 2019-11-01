a62e95be215ed83f37f5d883356f1fc3d6ce17a5
Frederik Nielsen <draxxdk@gmail.com>
2019-11-01 15:20:40 +0100

    dropdown filter

--- js/app/inputs/dropdown.js
+++ js/app/inputs/dropdown.js
@@ -28,7 +28,8 @@
                 if (text.indexOf('$$') === 0) {
                     li.attr('data-math', text);
                 }
-                return `<li data-id="${li.val()}"${li.is(':selected') ? ' class="selected"' : ''}>
+                return `
+<li data-id="${li.val()}"${li.is(':selected') ? ' class="selected"' : ''}>
     <div tabindex="0" class="theme-light">
         <label>${text}</label>
         <svg focusable="false"><use xlink:href="#svg-checkmark"></use></svg>
@@ -52,11 +53,31 @@
         <svg focusable="false"><use xlink:href="#svg-arrow"></use></svg>
     </div>
     <ul class="${theme}">
+        ${$this.hasClass('filter') ? `
+<li class="filter"><input type="text" placeholder="Filter"></li>
+<li class="no-results hidden"><div><label>No results</label></div></li>` : ''}
         ${$.makeArray($this.children()).map(liTemplate).join('')}
     </ul>
 </div>`;
             $this.after(html);
             html = $this.next();
+            if ($this.hasClass('filter')) {
+                let lis = html.find('li:not(.filter)'),
+                    noResults = html.find('li.no-results');
+                html.on('keyup', '.filter > input', (e) => {
+                    let any = lis.filter((i, li) => {
+                        if (li.innerText.indexOf(e.currentTarget.value) === -1) {
+                            li.classList.add('hidden');
+                            return false;
+                        } else {
+                            li.classList.remove('hidden');
+                            return true;
+                        }
+                    });
+
+                    any.length ? noResults.addClass('hidden') : noResults.removeClass('hidden');
+                });
+            }
 
             if ($this.hasClass('nowrap')) {
                 let top = html.children().eq(0),
@@ -73,7 +94,7 @@
                 layoutr.dropdown = dropdown;
                 dropdown.toggleClass('open');
             });
-            html.on('click', 'li', (e) => {
+            html.on('click', 'li:not(.filter)', (e) => {
                 let $that = $(e.currentTarget);
                 if (!$that.hasClass('selected')) {
                     console.log('test a');
