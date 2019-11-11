f525576669b0fc4101cf878bd3b6a73cbb3c05f1
Frederik Nielsen <draxxdk@gmail.com>
2019-11-11 19:15:54 +0100

    swiper fix

--- js/app/components/swiper.js
+++ js/app/components/swiper.js
@@ -15,66 +15,63 @@
             layoutr.promiseSwiper.then(() => {
                 swiper.each((i, e) => {
                     let $this = $(e);
-                    if (!$this.hasClass('loaded')) {
-                        $this.append('<div class="swiper-footer"></div>');
-                        $this.children('.swiper-footer').append('<div></div>');
+                    $this.append('<div class="swiper-footer"></div>');
+                    $this.children('.swiper-footer').append('<div></div>');
 
-                        let footer = $this.find('> .swiper-footer > div'),
-                            pagination = $this.hasClass('pagination'),
-                            dynamicBullets = $this.hasClass('dynamic-bullets'),
-                            navigation = $this.hasClass('navigation'),
-                            loop = layoutr.isTrue($this.attr('data-loop')),
-                            spaceBetween = $this.hasClass('space-between'),
-                            slidesPerView = layoutr.tryParseInt($this.attr('data-slides-per-view'), 1),
-                            breakpoints = layoutr.tryParseJSON($this.attr('data-breakpoints'), {});
+                    let footer = $this.find('> .swiper-footer > div'),
+                        pagination = $this.hasClass('pagination'),
+                        dynamicBullets = $this.hasClass('dynamic-bullets'),
+                        navigation = $this.hasClass('navigation'),
+                        loop = layoutr.isTrue($this.attr('data-loop')),
+                        spaceBetween = $this.hasClass('space-between'),
+                        slidesPerView = layoutr.tryParseInt($this.attr('data-slides-per-view'), 1),
+                        breakpoints = layoutr.tryParseJSON($this.attr('data-breakpoints'), {});
 
-                        footer.addClass('flex wrap gap-3 center');
+                    footer.addClass('flex wrap gap-3 center');
 
-                        if (pagination || dynamicBullets) {
-                            footer.append('<div class="swiper-pagination flex wrap gap-1 center vertical-center"></div>');
-                        }
-
-                        if (navigation) {
-                            footer.append('<button class="btn square circle prev theme-dark"><svg focusable="false"><use xlink:href="#svg-arrow"></use></svg></button>');
-                            footer.append('<button class="btn square circle next theme-dark"><svg focusable="false"><use xlink:href="#svg-arrow"></use></svg></button>');
-                        }
-
-                        if (spaceBetween) {
-                            spaceBetween = 16;
-                        }
-
-                        let swiper = new Swiper($this[0], {
-                            loop: loop,
-                            slidesPerView: slidesPerView,
-                            spaceBetween: spaceBetween,
-
-                            // If we need pagination
-                            pagination: {
-                                el: '.swiper-pagination',
-                                dynamicBullets: dynamicBullets
-                            },
-
-                            // Navigation arrows
-                            navigation: {
-                                nextEl: '.swiper-footer .next',
-                                prevEl: '.swiper-footer .prev'
-                            },
-
-                            // And if we need scrollbar
-                            scrollbar: {
-                                el: '.swiper-scrollbar'
-                            },
-                            breakpoints: breakpoints,
-                            on: {
-                                init: () => {
-                                    $this.addClass('loaded');
-                                },
-                                slideChange: () => {
-                                    layoutr.checkLazy($this.find('.lazy'));
-                                }
-                            }
-                        });
+                    if (pagination || dynamicBullets) {
+                        footer.append('<div class="swiper-pagination flex wrap gap-1 center vertical-center"></div>');
                     }
+                    if (navigation) {
+                        footer.append('<button class="btn square circle prev theme-dark"><svg focusable="false"><use xlink:href="#svg-arrow"></use></svg></button>');
+                        footer.append('<button class="btn square circle next theme-dark"><svg focusable="false"><use xlink:href="#svg-arrow"></use></svg></button>');
+                    }
+
+                    if (spaceBetween) {
+                        spaceBetween = 16;
+                    }
+
+                    let swiper = new Swiper($this[0], {
+                        loop: loop,
+                        slidesPerView: slidesPerView,
+                        spaceBetween: spaceBetween,
+
+                        // If we need pagination
+                        pagination: {
+                            el: '.swiper-pagination',
+                            dynamicBullets: dynamicBullets
+                        },
+
+                        // Navigation arrows
+                        navigation: {
+                            nextEl: '.swiper-footer .next',
+                            prevEl: '.swiper-footer .prev'
+                        },
+
+                        // And if we need scrollbar
+                        scrollbar: {
+                            el: '.swiper-scrollbar'
+                        },
+                        breakpoints: breakpoints,
+                        on: {
+                            init: () => {
+                                $this.addClass('loaded');
+                            },
+                            slideChange: () => {
+                                layoutr.checkLazy($this.find('.lazy'));
+                            }
+                        }
+                    });
                 });
             }).catch((e) => {
                 layoutr.showPopupAlert('Failed to load swiper', 'danger');
