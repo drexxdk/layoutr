8f32ac4b4c6723db24e0a214ed8f745bbf42ff49
Frederik Nielsen <draxxdk@gmail.com>
2020-03-27 01:13:35 +0100

    audio-visualiser update

--- js/app/components/audioVisualiser.js
+++ js/app/components/audioVisualiser.js
@@ -32,11 +32,14 @@
 <div class="position bottom right">
     <div>
         <div class="flex gap-2">
-            <button type="button" class="btn bars">
+            <button type="button" class="btn" data-type="bars" aria-label="Bars">
                 <svg focusable="false"><use xlink:href="#svg-audio-bars"></use></svg>
             </button>
-            <button type="button" class="btn wave">
-                <svg focusable="false"><use xlink:href="#svg-audio-wave"></use></svg>
+            <button type="button" class="btn" data-type="oscilloscope" aria-label="Oscilloscope">
+                <svg focusable="false"><use xlink:href="#svg-audio-oscilloscope"></use></svg>
+            </button>
+            <button type="button" class="btn" data-type="waveform" aria-label="Waveform">
+                <svg focusable="false"><use xlink:href="#svg-audio-waveform"></use></svg>
             </button>
         </div>
     </div>
@@ -44,15 +47,18 @@
 `);
                     audio = audio[0];
                     canvas = canvas[0];
+                    let buffer;
+                    fetch(audio.currentSrc)
+                        .then(response => response.arrayBuffer())
+                        .then(arrayBuffer => audioContext.decodeAudioData(arrayBuffer))
+                        .then(audioBuffer = (audioBuffer) => {
+                            buffer = normalizeData(filterData(audioBuffer));
+                        });
 
-                    element.find('button.bars').click(() => {
-                        element.attr('data-type', 'bars');
+                    element.find('button').click((e) => {
+                        let type = $(e.target).attr('data-type');
+                        element.attr('data-type', type);
                     });
-
-                    element.find('button.wave').click(() => {
-                        element.attr('data-type', 'wave');
-                    });
-
 
                     let setSize = () => {
                         WIDTH = canvas.clientWidth;
@@ -66,15 +72,41 @@
                         return ((HEIGHT - 256) * 100) / 256;
                     };
 
+                    const normalizeData = filteredData => {
+                        const multiplier = Math.pow(Math.max(...filteredData), -1);
+                        return filteredData.map(n => n * multiplier);
+                    }
+
+                    const filterData = audioBuffer => {
+                        const rawData = audioBuffer.getChannelData(0); // We only need to work with one channel of data
+                        const blockSize = Math.floor(rawData.length / SAMPLES); // the number of samples in each subdivision
+                        const filteredData = [];
+                        for (let i = 0; i < SAMPLES; i++) {
+                            let blockStart = blockSize * i; // the location of the first sample in the block
+                            let sum = 0;
+                            for (let j = 0; j < blockSize; j++) {
+                                sum = sum + Math.abs(rawData[blockStart + j]); // find the sum of all the samples in the block
+                            }
+                            filteredData.push(sum / blockSize); // divide the sum by the block size to get the average
+                        }
+                        return filteredData;
+                    };
+
                     let draw = () => {
                         if (!audio.paused) {
+                            analyser.minDecibels = -100;
+                            analyser.maxDecibels = -30;
+                            analyser.smoothingTimeConstant = 0.8;
+                            canvasContext.resetTransform();
+                            canvasContext.shadowBlur = 0;
+                            canvasContext.lineWidth = 1;
+                            canvasContext.clearRect(0, 0, WIDTH, HEIGHT);
                             x = 0;
-                            if (element.attr('data-type') === 'wave') {
+                            if (element.attr('data-type') === 'oscilloscope') {
                                 analyser.minDecibels = -90;
                                 analyser.maxDecibels = -10;
                                 analyser.smoothingTimeConstant = 0.85;
                                 analyser.getByteTimeDomainData(freqArr);
-                                canvasContext.clearRect(0, 0, WIDTH, HEIGHT);
                                 canvasContext.lineWidth = 2;
                                 canvasContext.shadowOffsetX = 0;
                                 canvasContext.shadowOffsetY = 0;
@@ -117,14 +149,10 @@
 
                                 canvasContext.lineTo(WIDTH, HEIGHT / 2);
                                 canvasContext.stroke();
-                            } else {
-                                analyser.minDecibels = -100;
-                                analyser.maxDecibels = -30;
-                                analyser.smoothingTimeConstant = 0.8;
+                            } else if (element.attr('data-type') === 'bars') {
                                 r = 0;
                                 g = 0;
                                 b = 255;
-                                canvasContext.clearRect(0, 0, WIDTH, HEIGHT);
                                 analyser.getByteFrequencyData(freqArr);
 
                                 for (let i = 0; i < INTERVAL; i++) {
@@ -170,11 +198,44 @@
                                     canvasContext.fillRect(x, HEIGHT - barHeight, (WIDTH / INTERVAL) - 1, barHeight);
                                     x = x + (WIDTH / INTERVAL);
                                 }
+                            } else if (element.attr('data-type') === 'waveform' && buffer) {
+                                canvasContext.translate(0, HEIGHT / 2);
+                                let width = WIDTH / buffer.length;
+                                for (let i = 0; i < buffer.length; i++) {
+                                    const x = width * i;
+                                    let height = buffer[i] * HEIGHT;
+                                    if (height < 0) {
+                                        height = 0;
+                                    } else if (height > HEIGHT / 2) {
+                                        height = height > HEIGHT / 2;
+                                    }
+
+                                    if (theme === 'theme-primary') {
+                                        canvasContext.strokeStyle = "#0072ED";
+                                    }
+                                    else if (theme === 'theme-success') {
+                                        canvasContext.strokeStyle = "#218838";
+                                    }
+                                    else if (theme === 'theme-danger') {
+                                        canvasContext.strokeStyle = "#dc3545";
+                                    }
+                                    else if (theme === 'theme-warning') {
+                                        canvasContext.strokeStyle = "#BE5A06";
+                                    } else {
+                                        canvasContext.strokeStyle = "#6F7780";
+                                    }
+                                    canvasContext.beginPath();
+                                    canvasContext.moveTo(x, 0);
+                                    canvasContext.lineTo(x, height);
+                                    canvasContext.lineTo(x, -height);
+                                    canvasContext.moveTo(x, 0);
+                                    canvasContext.lineTo(x + width, 0);
+                                    canvasContext.stroke();
+                                }
                             }
                         }
                         window.requestAnimationFrame(draw);
                     };
-
 
                     let WIDTH,
                         HEIGHT,
